40fc96163923c32a1ad7718292c1b17899c295eb
Tarfeehaww <hussaintarfeeha40@example.com>
2024-12-05 07:47:28 +0500

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer () {
+    return(
+        <div className="mt-16 py-6 text-center bg-black">
+        &#169; TarfeehaHussain | All rights reserved 2024.             
+    </div>
+
+
+  
+     
+    )}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,104 @@
+
+export default function Project() {
+    return (
+      
+  
+        <section className="text-gray-600 body-font pt-10">
+          <div className="text-center mb-6">
+            <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-lg sm:text-xl lg:text-5xl font-serif">My Projects</h1>
+          </div>
+  
+          <div id="projects" className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              <div className="p-4 md:w-1/3 transition-transform duration-300 ease-in-out transform hover:scale-110">
+                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="/images/countdown timer.jpg" alt="blog" />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Project:01</h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Countdown Timer</h1>
+                    <p className="leading-relaxed mb-3">A countdown timer in Next.js is a dynamic component that displays the time remaining until a specific event, updating in real-time using React hooks. It's perfect for adding interactivity to your portfolio for events or launches.</p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                      <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        <svg className="w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                          <circle cx="12" cy="12" r="3"></circle>
+                        </svg>1.2K
+                      </span>
+                      <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                        <svg className="w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>6
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div className="p-4 md:w-1/3 transition-transform duration-300 ease-in-out transform hover:scale-110">
+                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="/images/Static.JPG" alt="blog" />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Project:02</h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Static Resume Builder</h1>
+                    <p className="leading-relaxed mb-3">A simple resume builder app created using HTML and CSS. This project demonstrates the use of static HTML and CSS to create a clean and user-friendly resume layout.</p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div className="p-4 md:w-1/3 transition-transform duration-300 ease-in-out transform hover:scale-110">
+                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="/images/Dynamic.JPG" alt="blog" />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Project:03</h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Dynamic Resume Builder</h1>
+                    <p className="leading-relaxed mb-3">A dynamic resume builder app built with HTML, CSS, and JavaScript, allowing users to create and edit resumes with a modern and interactive interface.</p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div className="p-4 md:w-1/3 transition-transform duration-300 ease-in-out transform hover:scale-110">
+                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                  <img className="lg:h-48 md:h-36 w-full object-cover object-center" src="/images/Editable.JPG" alt="blog" />
+                  <div className="p-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Project:04</h2>
+                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Editable Resume Builder</h1>
+                    <p className="leading-relaxed mb-3">A resume builder with editable features using HTML, CSS, and JavaScript. This project allows users to dynamically edit and generate their resume.</p>
+                    <div className="flex items-center flex-wrap">
+                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                        <svg className="w-4 h-4 ml-2" viewBox="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+           </div>
+        </section>
+    );
+  }
+  
